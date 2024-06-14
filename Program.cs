00a0762f7bb89db6f69c5ff073134fce986ed236
erykt <erykt@WARMACHINE>
2024-06-14 18:15:54 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿{
+    string name = "Ewa"; 
+    string gender = "Female"; 
+    var age = 30; 
+
+    
+    if (gender == "Female" && age <= 30) 
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat"); 
+    }
+
+    else if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa, lat 30"); 
+    }
+    
+    else if (gender == "Male" && age < 18) 
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna"); 
+    }
+
+    else
+    {
+        Console.WriteLine("DANE NIEPOPRAWNE"); 
+    }
+}
